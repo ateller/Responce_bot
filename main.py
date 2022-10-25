f0765b9e9b4e9921cd18b5aebab7b90705d8e755
Антон <tony.tel@yandex.ru>
2022-10-26 00:13:45 +0300

    Fixed one thing and added pause feature

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from telethon import TelegramClient, events
+from telethon import TelegramClient, events, types
 from config import api_id, api_hash, replies
 import logging
 import random
@@ -6,6 +6,7 @@ import random
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.WARNING) #not sure if I need this, but API guide said I do
 
 client = TelegramClient('anon', api_id, api_hash)
+paused = False
 
 async def main():
 
@@ -14,13 +15,29 @@ async def main():
 
 @client.on(events.NewMessage)
 async def reply_on(event):
+
+    global paused
+
     if event.forward is not None: #do not reply to forwarded messages
         return
     
     author = await event.get_sender()
 
-    if author.is_self is True and event.raw_text == 'Упал?': #To check if the thing is running
-        await event.reply("Не упал")
+    if not isinstance(author, types.User): #So we don't get error when trying to access User attributes with Channels which doesn't have it
+        return
+
+    if author.is_self is True:
+        if event.raw_text == 'Упал?': #To check if the thing is running
+            await event.reply("Не упал")
+        elif event.raw_text == 'Pause':
+            paused = True
+            await event.reply("Paused")
+        elif event.raw_text == 'Resume':
+            paused = False
+            await event.reply("Resumed")
+        return           
+
+    if paused:
         return
 
     rep_list = replies.get(author.username)
